4d16770e3d80be2dad9c826682695570cb61a9b5
edo1217 <edoardo.incontri.ab1@managementsolutions.com>
2025-01-29 09:53:20 -0600

    nuevos cambios

---
+++ features/pageobjects/mapfre.page.js
@@ -0,0 +1,97 @@
+const { $ } = require ('@wdio/globals')
+const Page = require ('./page.js');
+
+class MapfrePage extends Page{
+
+    get selectCarBrand() {
+        return $('#VehicleMake_Control');
+    }
+    get selectCarYear() {
+        return $('#VehicleYear_Control');
+    }
+    get selectCarModel() {
+        return $('#VehicleModel_Control');
+    }
+    /* get selectCarVersion() {
+        return $('#VehicleVersion_Control');
+    } */
+    //Info Personal
+    get selectBornDay() {
+        return $('#DriverBirthDate_Day');
+    }
+    get selectBornMonth() {
+        return $('#DriverBirthDate_Month');
+    }
+    get selectBornYear() {
+        return $('#DriverBirthDate_Year');
+    }
+    get selectGender() {
+        return $('#DriverGender_Control');
+    }
+    get inputCp() {
+        return $('#driverzipcode');
+    }
+    get inputName() {
+        return $('#Name');
+    }
+    get inputMail() {
+        return $('#EmailId');
+    }
+    get inputNumber() {
+        return $('#Telephone');
+    }
+    //Terminos y condiciones
+    get selectTerm() {
+        return $('#TermAndCondition');
+    }
+    get inputNumber() {
+        return $('#Telephone');
+    }
+    get btnContinue() {
+        return $('#btnSubmitForm');
+    }
+    /* get btnDetails(){
+        return $('#CoverLink1_QP_MapfreDirectoLimited');
+    } */
+
+     //Comportamiento
+    async selectCarBrand(text) {
+        await this.selectCarBrand.selectByVisibleText(text);
+    }
+    async selectCarBrand(brand) {
+        await this.selectCarBrand.waitForExist({ timeout: 5000 });
+        await this.selectCarBrand.selectByVisibleText(brand);
+    }
+    async selectCarYear(year) {
+        await this.selectCarYear.selectByVisibleText(text);
+    }
+    async selectCarModel(model) {
+        await this.selectCarModel.selectByVisibleText(model);
+    }
+    async fillPersonalInfo(day, month, year, gender) {
+        await this.selectBornDay.waitForExist({ timeout: 5000 });
+        await this.selectBornDay.selectByVisibleText('10');
+        await this.selectBornMonth.selectByVisibleText('enero');
+        await this.selectBornMonth.selectByVisibleText('1977');
+        await this.selectGender.waitForExist({ timeout: 5000 });
+        await this.selectGender.selectByVisibleText('conductor');
+        await this.inputCp.setValue('06140');
+        await this.inputName.setValue('Jose');
+        await this.inputMail.setValue('prueba@gmail.com');
+        await this.inputNumber.setValue('5555555564');
+        await this.btnContinueQuote.click();
+
+    }
+    async acceptTerm() {
+        await this.selectTerm.waitForExist({ timeout: 5000 });
+        await this.selectTerm.click();
+        await this.btnContinue.click();
+
+    }
+    open() {
+        return super.openMapfre();
+    }
+
+}
+
+module.exports =new MapfrePage();
